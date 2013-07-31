5343b853dfa24088c96df002112e9245f26bbef7
Toshiyuki Nakamura <yukidarake4@gmail.com>
2013-07-31 11:17:43 +0900

    change shell script style

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [ ! -d ~/.vim/bundle ]; then
     git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 fi
 
-for file in ${DOT_FILES[@]}
-do
+for file in ${DOT_FILES[@]}; do
     ln -s $HOME/github/dotfiles/$file $HOME/$file
 done
+

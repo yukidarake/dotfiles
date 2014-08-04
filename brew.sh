3be4644980d7506bc44538539f3b2dc5141d75af
Toshiyuki Nakamura <yukidarake4@gmail.com>
2014-08-04 17:04:23 +0900

    Brewfileが非推奨になったため普通のシェルに変更

---
+++ brew.sh
@@ -0,0 +1,69 @@
+# Add Repository
+brew tap homebrew/versions 
+brew tap homebrew/dupes 
+brew tap homebrew/nginx 
+brew tap caskroom/cask 
+brew tap caskroom/versions
+brew tap yukidarake/homebrew-793 
+brew tap peco/peco 
+
+# Packages
+brew install boot2docker
+brew install brew-cask 
+brew install casperjs 
+brew install coreutils 
+brew install curl 
+brew install git 
+brew install go 
+brew install hg 
+brew install httpie 
+brew install hub 
+brew install lua 
+brew install lua52 
+brew install luajit 
+brew install mongodb22 
+brew install mobile-shell 
+#brew install nginx 
+brew install nginx-full 
+brew install ngrep 
+brew install peco 
+brew install phantomjs 
+brew install plenv 
+brew install pyenv 
+brew install rbenv 
+brew install nodebrew 
+brew install readline 
+brew install reattach-to-user-namespace 
+brew install subversion 
+brew install the_silver_searcher 
+brew install tig 
+brew install tmux 
+brew install w3m 
+brew install wget 
+brew install zsh 
+
+# Add GUI Applications
+brew cask install alfred 
+brew cask install appcleaner 
+brew cask install bettertouchtool 
+brew cask install chromium 
+brew cask install day-o 
+brew cask install firefox 
+brew cask install google-chrome 
+brew cask install google-japanese-ime 
+brew cask install hipchat 
+brew cask install iterm2 
+brew cask install karabiner 
+# brew cask install limechat 
+brew cask install macvim-kaoriya 
+brew cask install nosleep 
+brew cask install skype 
+brew cask install sourcetree 
+brew cask install waterroof 
+brew cask install xtrafinder 
+brew cask install virtualbox 
+brew cask install vagrant 
+brew cask install macvim-kaoriya
+
+brew cask alfred link 
+

b42179900cf67d9d472c40f7ea1caec116181f05
Toshiyuki Nakamura <yukidarake4@gmail.com>
2016-03-22 13:12:02 +0900

    ちょいと変えた

--- README.md
+++ README.md
@@ -1,38 +1,48 @@
-# Usage
+## Usage
 初回のみ
-```
+```sh
+# homebrewをインストール
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# ghqでこのプロジェクトを落とす
+cat <<. >> ~/.gitconfig
+[ghq]
+  root = ~/src
+. 
+brew install ghq
+ghq get https://github.com/yukidarake/dotfiles.git
+cd ~/src/github.com/yukidarake/dotfiles
 ./setup.sh # いろいろ落としてくる
 ./brew.sh # Homebrew, HomebrewCaskのもろもろインストール
 ```
 
-# Macでする設定
+## Macでする設定
 
-## タブでボタンを選択できるようにする
+### タブでボタンを選択できるようにする
 システム環境設定→キーボード→キーボードショートカット→すべてのコントロール
 
-## COMMAND + F1をウィンドウ切り替えにする
+### COMMAND + F1をウィンドウ切り替えにする
 システム環境設定→キーボード→キーボードショートカット
 
-## ダブルタップでドラッグにする
+### ダブルタップでドラッグにする
 システム環境設定→アクセシビリティ→マウスとトラックパッド→トラックパッドオプション→ドラッグを有効にする
 
-## CTRL + ホイール回してズーム
+### CTRL + ホイール回してズーム
 アクセシビリティ→Zoom
 
-# dashboardを無効にする
+## dashboardを無効にする
 ```
 defaults write com.apple.dashboard mcx-disabled -boolean true
 killall Dock
 ```
 
-# アニメーション高速化
+## アニメーション高速化
 ```
-defaults write com.apple.dock expose-animation-duration -float 0.15
+defaults write com.apple.dock expose-animation-duration -float 0.01
 killall Dock
 ```
 
-# COMMAND + TAB時の切り替えにアプリアイコンが出ないようにする
-## Info.plistの編集
+## COMMAND + TAB時の切り替えにアプリアイコンが出ないようにする
+### Info.plistの編集
 下記追記
 
 ```xml
--- brew.sh
+++ brew.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Add Repository
 brew tap homebrew/versions 
 brew tap homebrew/dupes 
@@ -6,6 +7,7 @@ brew tap caskroom/cask
 brew tap caskroom/versions
 
 # Packages
+brew install zsh --disable-etcdir 
 brew install brew-cask 
 brew install coreutils 
 brew install curl 
@@ -15,26 +17,23 @@ brew install hg
 brew install hub 
 brew install lua 
 brew install luajit 
-brew install mongodb
 brew install mobile-shell 
-#brew install nginx 
+brew install mongodb
 brew install nginx-full 
 brew install ngrep 
+brew install nodebrew 
 brew install peco 
 brew install plenv 
 brew install pyenv 
 brew install rbenv 
-brew install ruby-build
-brew install nodebrew 
 brew install readline 
 brew install reattach-to-user-namespace 
-brew install subversion 
+brew install ruby-build
+brew install shellcheck 
 brew install the_silver_searcher 
 brew install tig 
 brew install tmux 
-brew install w3m 
 brew install wget 
-brew install --disable-etcdir zsh 
 
 # git diff
 ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight 
@@ -46,17 +45,14 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 brew cask install alfred 
 brew cask install appcleaner 
 brew cask install bettertouchtool 
-brew cask install chromium 
 brew cask install day-o 
-brew cask install firefox 
 brew cask install google-chrome 
 brew cask install google-japanese-ime 
-brew cask install iterm2 
 brew cask install karabiner 
+brew cask install macvim-kaoriya
 brew cask install nosleep 
 brew cask install skype 
 brew cask install slack 
-brew cask install waterroof 
-brew cask install virtualbox 
 brew cask install vagrant 
-brew cask install macvim-kaoriya
+brew cask install virtualbox 
+brew cask install waterroof 

92e6950d39eb0f796c4ecd6ab4923fe2c7c6a82f
Toshiyuki Nakamura <yukidarake4@gmail.com>
2017-05-03 14:58:01 +0900

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,18 @@
 set fish_greeting 
 
-set -x GOPATH $HOME
-set -x EDITOR vim
+# set PATH \
+#   ~/.nodebrew/current/bin \
+#   ~/.pyenv/shims \
+#   ~/.plenv/shims \
+#   ~/.rbenv/shims \
+#   $GOPATH/bin \
+#   /usr/local/bin \
+#   $PATH
 set -x TERM xterm-256color
-set -x PATH /usr/local/bin $PATH
-set -x PATH ~/.nodebrew/current/bin $PATH
-set -x PATH ~/.pyenv/shims $PATH
-set -x PATH ~/.plenv/shims $PATH
-set -x PATH ~/.rbenv/shims $PATH
-set -x PATH $GOPATH/bin $PATH
 
+set -U EDITOR vim
 set -U FZF_TMUX 1
+set -U GOPATH $HOME
 set -U Z_CMD "j"
 set -U Z_DATA "$HOME/.z/.z"
 
@@ -22,3 +24,4 @@ alias ..... 'cd ../../../..'
 alias cp 'cp -i'
 alias rm 'rm -i'
 alias mv 'mv -i'
+alias nv nvim

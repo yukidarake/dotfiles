847b6dd1806badfb446ad1ba2fdf49c8f78304f7
Toshiyuki Nakamura <yukidarake4@gmail.com>
2018-06-10 09:30:29 +0900

    config.sh内でユニバーサルは使わないようにしよう

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,18 @@
 set fish_greeting 
 
-# set PATH \
-#   ~/.nodebrew/current/bin \
-#   ~/.pyenv/shims \
-#   ~/.plenv/shims \
-#   ~/.rbenv/shims \
-#   $GOPATH/bin \
-#   /usr/local/bin \
-#   $PATH
+set -x PATH \
+  ~/.nodebrew/current/bin \
+  ~/.pyenv/shims \
+  ~/.rbenv/shims \
+  $GOPATH/bin \
+  $PATH
 set -x TERM xterm-256color
 
-set -U EDITOR vim
-set -U FZF_TMUX 1
-set -U GOPATH $HOME
-set -U Z_CMD "j"
-set -U Z_DATA "$HOME/.z/.z"
+set -x EDITOR vim
+set -x FZF_TMUX 0
+set -x GOPATH $HOME
+set -x Z_CMD "j"
+set -x Z_DATA "$HOME/.z/.z"
 
 alias tmux 'tmux -2'
 alias .. 'cd ..'
@@ -24,4 +22,3 @@ alias ..... 'cd ../../../..'
 alias cp 'cp -i'
 alias rm 'rm -i'
 alias mv 'mv -i'
-alias nv nvim

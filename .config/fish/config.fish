a4154d42a89ba806a08a7842d178e63aeb1b7b70
Toshiyuki Nakamura <yukidarake@users.noreply.github.com>
2020-03-29 11:56:00 +0900

    ちょい変更変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 set fish_greeting 
 
+export LANG=ja_JP.UTF-8
+
 if not string match -q '*shims*' -- $PATH
   set -x PATH \
     ~/.nodebrew/current/bin \
@@ -13,7 +15,7 @@ set -x FZF_TMUX 0
 set -x Z_CMD "j"
 set -x Z_DATA "$HOME/.z/.z"
 
-alias tmux 'tmux -2'
+alias tmux 'tmux -2 -u'
 alias .. 'cd ..'
 alias ... 'cd ../..'
 alias .... 'cd ../../..'
@@ -24,3 +26,4 @@ alias mv 'mv -i'
 alias l ll
 alias vi vim
 
+source /usr/local/opt/asdf/asdf.fish

6862a9391c55490b803a5c5595de21e0ab622113
Toshiyuki Nakamura <yukidarake@users.noreply.github.com>
2020-03-29 12:59:01 +0900

    いろいろ足したった

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,13 @@
-set fish_greeting 
+set fish_greeting
 
-export LANG=ja_JP.UTF-8
-
-if not string match -q '*shims*' -- $PATH
+if not string match -q '*go/bin*' -- $PATH
   set -x PATH \
-    ~/.nodebrew/current/bin \
-    ~/.pyenv/shims \
     ~/go/bin \
     $PATH
 end
 set -x TERM xterm-256color
+set -x PIPENV_VENV_IN_PROJECT true
+
 set -x EDITOR vim
 set -x FZF_TMUX 0
 set -x Z_CMD "j"
@@ -20,10 +18,25 @@ alias .. 'cd ..'
 alias ... 'cd ../..'
 alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
+alias l 'ls -la'
 alias cp 'cp -i'
 alias rm 'rm -i'
 alias mv 'mv -i'
-alias l ll
-alias vi vim
+alias vi 'vim'
+
+function pfd -d "Return the path of the frontmost Finder window"
+  osascript 2>/dev/null -e '
+    tell application "Finder"
+      return POSIX path of (target of window 1 as alias)
+    end tell'
+end
+
+function cdf -d "cd to the current Finder directory"
+  cd (pfd)
+end
+
+function fssh -d "Fuzzy-find ssh host via rg and ssh into it"
+  rg --ignore-case '^host [^*]' ~/.ssh/config ~/.ssh/conf.d/hosts/* | cut -d ' ' -f 2 | fzf -m --tac --cycle | xpanes --ssh
+end
 
 source /usr/local/opt/asdf/asdf.fish

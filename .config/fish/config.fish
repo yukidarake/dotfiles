acea2411416bdae6fac3c691bc5e77697022481c
Toshiyuki Nakamura <yukidarake@users.noreply.github.com>
2022-10-27 09:41:30 +0900

    Change fish config
    diff --git a/.config/fish/config.fish b/.config/fish/config.fish
    index 84fe4a6..2af91c8 100644
    --- a/.config/fish/config.fish
    +++ b/.config/fish/config.fish
    @@ -1,29 +1,24 @@
    -set fish_greeting
    -
    -fish_add_path ~/go/bin
    -set -x TERM xterm-256color
    -set -x PIPENV_VENV_IN_PROJECT true
    -set -x LANG ja_JP.UTF-8
    -set -x EDITOR vim
    -set -x FZF_TMUX 0
    -set -x Z_CMD "j"
    -set -x Z_DATA "$HOME/.z/.z"
    -
     alias tmux 'tmux -2 -u'
     alias .. 'cd ..'
     alias ... 'cd ../..'
     alias .... 'cd ../../..'
     alias ..... 'cd ../../../..'
    +alias ..... 'cd ../../../../..'
     alias l 'ls -la'
    +alias ll 'ls -la'
     alias cp 'cp -i'
     alias rm 'rm -i'
     alias mv 'mv -i'
    -alias vi 'vim'
    +alias vi 'nvim'
    +
    +set -g fish_greeting
    +set -x TERM xterm-256color
    +set -x LANG ja_JP.UTF-8
    +set -x EDITOR vi
    +set -x FZF_TMUX 0
    
    -if not functions -q fisher
    -    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
    -    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
    -    fish -c fisher
    +if type -q zoxide
    +  zoxide init fish | source
     end
    
     function pfd -d "Return the path of the frontmost Finder window"
    @@ -40,10 +35,3 @@ end
     function fssh -d "Fuzzy-find ssh host via rg and ssh into it"
       rg --ignore-case '^host [^*]' ~/.ssh/config ~/.ssh/conf.d/hosts/* | cut -d ' ' -f 2 | fzf -m --tac --cycle | xpanes --ssh
     end
    -
    -if test -f /opt/homebrew/opt/asdf/asdf.fish
    -  source /opt/homebrew/opt/asdf/asdf.fish
    -else
    -  echo '/opt/homebrew/opt/asdf/asdf.fish not found'
    -end
    -
    diff --git a/.config/fish/fishfile b/.config/fish/fish_plugins
    similarity index 55%
    rename from .config/fish/fishfile
    rename to .config/fish/fish_plugins
    index e07164f..8f2a27a 100644
    --- a/.config/fish/fishfile
    +++ b/.config/fish/fish_plugins
    @@ -1,4 +1,3 @@
     rafaelrinaldi/pure
     decors/fish-ghq
    -jethrokuan/z
    -jethrokuan/fzf
    +PatrickF1/fzf.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,24 @@
-set fish_greeting
-
-fish_add_path ~/go/bin 
-set -x TERM xterm-256color
-set -x PIPENV_VENV_IN_PROJECT true
-set -x LANG ja_JP.UTF-8
-set -x EDITOR vim
-set -x FZF_TMUX 0
-set -x Z_CMD "j"
-set -x Z_DATA "$HOME/.z/.z"
-
 alias tmux 'tmux -2 -u'
 alias .. 'cd ..'
 alias ... 'cd ../..'
 alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
+alias ..... 'cd ../../../../..'
 alias l 'ls -la'
+alias ll 'ls -la'
 alias cp 'cp -i'
 alias rm 'rm -i'
 alias mv 'mv -i'
-alias vi 'vim'
+alias vi 'nvim'
 
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+set -g fish_greeting
+set -x TERM xterm-256color
+set -x LANG ja_JP.UTF-8
+set -x EDITOR vi
+set -x FZF_TMUX 0
+
+if type -q zoxide
+  zoxide init fish | source
 end
 
 function pfd -d "Return the path of the frontmost Finder window"
@@ -40,10 +35,3 @@ end
 function fssh -d "Fuzzy-find ssh host via rg and ssh into it"
   rg --ignore-case '^host [^*]' ~/.ssh/config ~/.ssh/conf.d/hosts/* | cut -d ' ' -f 2 | fzf -m --tac --cycle | xpanes --ssh
 end
-
-if test -f /opt/homebrew/opt/asdf/asdf.fish
-  source /opt/homebrew/opt/asdf/asdf.fish
-else 
-  echo '/opt/homebrew/opt/asdf/asdf.fish not found'
-end
-

7315fceb704c8632f4b04154194491bdd0a999a5
Toshiyuki Nakamura <yukidarake@users.noreply.github.com>
2025-09-30 16:51:38 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
-#!/bin/bash 
+#!/usr/bin/env zsh
 set -euxo pipefail
 
 cd "$(dirname "$0")"
 
-if ! type brew >/dev/null 2>&1; then
+if ! type brew > /dev/null 2>&1; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
   brew bundle
 fi
@@ -24,6 +23,8 @@ DOT_FILES=(
   .ideavimrc
   .tmux.conf
   .terraformrc
+  .zprofile
+  .zshrc
 )
 for file in "${DOT_FILES[@]}"; do
   if [ ! -h "$HOME/$file" ]; then
@@ -45,19 +46,8 @@ for file in "${CONFIG_FILES[@]}"; do
   fi
 done
 
-if type fisher >/dev/null 2>&1; then
+if type fisher > /dev/null 2>&1; then
   fisher
-fi
-
-# key binding
-if [ ! -f ~/Library/KeyBindings/DefaultKeyBinding.dict ]; then
-  mkdir ~/Library/KeyBindings
-  cat << _EOT_ >> ~/Library/KeyBindings/DefaultKeyBinding.dict 2>&1
-{
-  "^w"="deleteWordBackward:";
-  "^u"="deleteToBeginningOfParagraph";
-}
-_EOT_
 fi
 
 # git
@@ -73,6 +63,7 @@ git config --global alias.p 'pull --prune'
 git config --global alias.f 'fetch --prune'
 git config --global merge.ff false
 git config --global pull.ff only
+git config --global user.email "yukidarake@users.noreply.github.com"
 
 # key repeat
 defaults write -g InitialKeyRepeat -int 10
@@ -81,3 +72,5 @@ defaults write -g KeyRepeat -int 1
 # https://qiita.com/ucan-lab/items/c1a12c20c878d6fb1e21
 defaults write com.apple.finder AppleShowAllFiles -boolean true
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+defaults write com.apple.AppleMultitouchTrackpad Dragging -bool true
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 2
